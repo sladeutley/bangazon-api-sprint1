ef8c02c75f4130da3b5abeeaa0f9f3f0b58ef01b
krieger27 <krieger@vols.utk.edu>
2018-03-21 15:03:29 -0500

    payment route params with getAll and getOne

---
+++ routes/paymentTypes.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const { Router } = require("express");
+const router = Router();
+const {
+  getPaymentTypes,
+  getOnePaymentType
+} = require("../controllers/paymentTypeCtrl");
+
+router.get("/paymentTypes", getPaymentTypes);
+router.get("/paymentTypes/:id", getOnePaymentType);
+// router.post("/paymentTypes", addPaymentType);
+// router.put("/paymentTypes/:id", editPaymentType);
+// router.delete("/paymentTypes/:id", deletePaymentType);
+
+module.exports = router;
