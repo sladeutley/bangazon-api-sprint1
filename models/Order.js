b5b4903087a7fede0ebe1fbc7ae3d1af566f55a2
BrandonAllenGreen <green04@gmail.com>
2018-03-21 12:35:50 -0500

    saving model file

--- models/Order.js
+++ models/Order.js
@@ -4,9 +4,7 @@ const db = new sqlite3.Database('./db/bangazon.sqlite');
 
 module.exports.getAll = () => {
   return new Promise( (resolve, reject) => {
-    db.all(`SELECT movies.*, directors.name AS director
-    FROM movies
-    JOIN directors ON director_id = directors.dir_id`,
+    db.all(`SELECT * FROM products`,
     (err, orders) => {
       if (err) return reject(err);
       resolve(orders);
