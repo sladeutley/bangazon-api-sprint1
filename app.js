b3d0f0f2e5bdcf1c29a8cd992f4ee00f0465a2c0
krieger27 <krieger@vols.utk.edu>
2018-03-23 10:52:07 -0500

    setup

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
-'use strict';
-
+const express = require("express");
+const app = express();
 const bodyParser = require('body-parser');
+const routes = require("./routes/");
+console.log("Hello from Express");
 
+// middleware stack starts here
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use("/api/v1", routes);
+
+// TODO: Add error handler
+app.use((req, res, next) => {
+  let err = new Error("Resource Not Found");
+  console.log("404 handler");
+  err.status = 404;
+  next(err);
+});
+app.use((err, req, res, next) => {
+  // one error handler to rule them all
+  res.json({
+    message: "Generic Error Message",
+    err: err.message
+  });
+});
+
+const port = process.env.PORT || 3000; //this is the server port (name) we're creating (e.x. localhost:8080)
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
