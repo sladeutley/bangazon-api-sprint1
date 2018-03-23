19c5b3b8c10b496855efc24756f9476ab4ad091a
Slade Utley <slade.utley@gmail.com>
2018-03-23 16:49:15 -0500

    created controller for computer data to retrieve  the data needed

--- routes/computers.js
+++ routes/computers.js
@@ -2,10 +2,10 @@ const { Router } = require("express");
 const compRouter = Router();
 const { getComputers, getOneComputer, addOneComputer, editComputerInfo, deleteOneComputer } = require("../controllers/compCtrl");
 
-prodsRouter.get(`/computers`, getcomputers);
-prodsRouter.get(`/computers/:id`, getOneComputer);
-prodsRouter.post(`/computers`, addOneComputer); //its fines they're the same route
-prodsRouter.put(`/computers/:id`, editComputerInfo);
-prodsRouter.delete(`/computers/:id`, deleteOneComputer);
+compRouter.get(`/computers`, getComputers);
+compRouter.get(`/computers/:id`, getOneComputer);
+compRouter.post(`/computers`, addOneComputer); //its fines they're the same route
+compRouter.put(`/computers/:id`, editComputerInfo);
+compRouter.delete(`/computers/:id`, deleteOneComputer);
 
 module.exports = compRouter;
